2fab7365aa1ca789526b186b539accddd22bb96e
arisia <starlights2283@gmail.com>
2023-10-07 10:33:11 +0900

    add kr page

---
+++ app/kr/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+export default function kr(){
+    return (
+        <div>
+            <title>RaunDoran - KR</title>
+            <div className="home">
+                <div className="logo"></div>
+                <div className="unit"></div>
+            </div>
+            <div className="menu">
+                <p className="space">공모전</p>
+                <p className="space">자유</p>
+                <p className="space">랭킹</p>
+                <p className="space">웹 만화</p>
+                <p className="space as">내 도서관</p>
+            </div>
+            <div className="banner">
+                
+            </div>
+        </div>
+    )
+}
